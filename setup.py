7bd311ba3550a76d4c5d4130ca5cf7e275beb4ec
Paul Barajas <paul.barajas@linux.com>
2018-04-28 22:06:09 -0500

    adding file structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Mood-Sense',
+    version='1.0',
+    description='Python Flask Training',
+    author='Paul Barajas',
+    author_email='paul.barajas@linux.com',
+    url='https://github.com/BarajasPaul/Mood-Sense',
+    packages=['flask'],
+)
